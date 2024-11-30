aab447dafd72ca2158d777844e27577262ad3bef
jsburbano <joans.burbano@gmail.com>
2024-11-30 03:24:32 -0500

    feat: initial  landing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,19 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next'
+import './globals.css'
 
 export const metadata: Metadata = {
-  title: "Finca La Pacunga",
-  description: "Finca La Pacunga, productora apicola de miel de abejas y derivados",
-};
+  title: 'Finca La Pacunga',
+  description: 'Finca La Pacunga, productora apicola de miel de abejas y derivados',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="es">
-      <body
-      className="bg-orange-100"
-      >
-        {children}
-      </body>
+      <body className="bg-gradient-to-tr from-zinc-950 to-zinc-800 from-15% min-h-screen">{children}</body>
     </html>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
     <div className="flex-1">
       {/* HERO BANNER */}
-      <section className="bg-gradient-to-tl from-zinc-900 to-[#3d3725] from-15% min-h-[480px] flex">
-        <img
-          src="https://www.lacostena.com.mx/media/uploads/Products/miel-abeja-360g_v2.png"
+      <section className="bg-gradient-to-tl from-zinc-900 to-[#3d3725] from-15% min-h-min max-w-[1280px] flex mx-auto">
+        {/* <img src="/hero.jpg" alt="Tarro de miel" className="h-full w-full" /> */}
+        <Image
+          src="/hero.jpg"
           alt="Tarro de miel"
-          className="w-auto h-[400px]"
+          className="h-full w-full"
+          objectFit="cover"
+          width={1366}
+          height={768}
         />
-        <article className="text-white text-center min-w-[250px] max-w-[550] w-[50%] m-auto">
-          <h1 className="text-5xl font-bold text-amber-300">Honey</h1>
+      </section>
+      <section className="bg-bone min-h-[700px] max-w-[1280px] flex mx-auto">
+        <article className="text-black text-center min-w-[250px] max-w-[550] w-[50%] m-auto">
+          <h1 className="text-5xl font-bold text-amber-900">100% {`\n`} NATURAL</h1>
           <p className="text-xs">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem consequatur voluptatum maiores
             ipsam laboriosam fuga deleniti suscipit accusamus saepe sapiente quaerat doloribus, odit rem sit?
@@ -18,12 +26,20 @@ export default function Home() {
             consectetur maiores ad eum illum possimus at.
           </p>
         </article>
-        <img
-          src="https://www.lacostena.com.mx/media/uploads/Products/miel-abeja-360g_v2.png"
-          alt="Tarro de miel"
-          className="w-auto h-[400px]"
-        />
       </section>
+      <footer className="bg-gradient-to-tl from-zinc-900 to-[#3d3725] from-15% min-h-[200px] max-w-[1280px] flex mx-auto">
+        <article className="text-black text-center min-w-[250px] max-w-[550] w-[50%] m-auto">
+          <h1 className="text-5xl font-bold text-amber-900">100% {`\n`} NATURAL</h1>
+          <Image src="/logo.webp" alt="Logo de la empresa" width={200} height={200} className="m-auto" />
+          <p className="text-xs">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem consequatur voluptatum maiores
+            ipsam laboriosam fuga deleniti suscipit accusamus saepe sapiente quaerat doloribus, odit rem sit?
+            Eaque pariatur assumenda delectus omnis! Aut non minima quisquam tenetur, repudiandae nulla cumque
+            atque culpa in, consequatur vero exercitationem, maxime ipsam assumenda sit ea. Totam nam id quis
+            consectetur maiores ad eum illum possimus at.
+          </p>
+        </article>
+      </footer>
     </div>
   )
 }
